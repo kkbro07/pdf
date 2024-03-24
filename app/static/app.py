b07badf6496fdc1e11cf4f2fd203057998c164ca
kkbro07 <128019029+kkbro07@users.noreply.github.com>
2024-03-24 11:41:42 +0530

    Add files via upload

---
+++ app/static/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for
+from PyPDF2 import PdfReader, PdfWriter
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/merge', methods=['POST'])
+def merge():
+    if request.method == 'POST':
+        pdf_files = request.files.getlist('pdf_files')
+        merged_pdf = PdfWriter()
+
+        for file in pdf_files:
+            pdf = PdfReader(file)
+            for page in pdf.pages:
+                merged_pdf.add_page(page)
+
+        with open('static/merged_pdf.pdf', 'wb') as output_pdf:
+            merged_pdf.write(output_pdf)
+
+        return redirect(url_for('result'))
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
